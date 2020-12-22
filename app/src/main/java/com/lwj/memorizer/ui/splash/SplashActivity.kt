1fea910d85f3f541a8c7466f96cdeab57cc5f68f
wjlim2 <sysoftwjlim2@gmail.com>
2020-12-22 16:14:29 +0900

    set splash activity

--- app/src/main/java/com/lwj/memorizer/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/lwj/memorizer/ui/splash/SplashActivity.kt
@@ -1,18 +1,32 @@
 package com.lwj.memorizer.ui.splash
 
+import android.content.Intent
 import com.lwj.memorizer.R
 import com.lwj.memorizer.base.BaseActivity
 import com.lwj.memorizer.databinding.ActivitySplashBinding
-import com.orhanobut.logger.Logger
+import com.lwj.memorizer.ui.login.LoginActivity
+import kotlinx.coroutines.*
 
 class SplashActivity : BaseActivity<ActivitySplashBinding, SplashViewModel>(
     R.layout.activity_splash,
     SplashViewModel::class.java
 ) {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun start() {
+        activityScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finishAffinity()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityScope.cancel()
     }
 
     override fun onObserve() {
     }
-
 }
